17419ec2e88fb75af7c9e0c895c160c13314ee4f
Yoav Hirsch <yoavjhirsch@gmail.com>
2020-06-17 19:49:20 -0400

    Updated data at Wed Jun 17 19:49:20 EDT 2020

--- cvStats.pl
+++ cvStats.pl
@@ -381,7 +381,7 @@ sub printCsvNarrow {
 
 	    #reduce file size so we can import into goolgle sheets:
 	    next if (exists($r->{_fake}));
-	    next if ($r->{cases} < 1200);
+	    next if ($r->{cases} < 1300);
 	    
 	    print FILE "$key,$locid,$date";
 	    for my $stat (@stats) {
